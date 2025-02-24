be1f004df475d35199f7c6d01fde944d693fa2db
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-02-24 21:44:32 +0700

    исправление замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,15 @@ const placesList = document.querySelector('.places__list');
 const createCard = (imageCard, nameCard, deleteCard) => {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__image').src = imageCard;
+  cardElement.querySelector('.card__image').alt = 'пейзаж';
   cardElement.querySelector('.card__title').textContent = nameCard;
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));  
-  placesList.append(cardElement);
+  return cardElement;
 };  
 
 const deleteCard = function(cardElement) {
     cardElement.remove();
 };
  
-for (let i = 0; i < initialCards.length; i++) {
-   createCard(initialCards[i].link, initialCards[i].name, deleteCard);
-};
+initialCards.forEach(element => {placesList.append(createCard(element.link, element.name, deleteCard))});
