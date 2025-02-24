0a58fc672a7c9138f640fca316f58d3e6ad3a90e
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-02-24 11:10:30 +0700

    версия для отпревки на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+const createCard = (imageCard, nameCard, deleteCard) => {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = imageCard;
+  cardElement.querySelector('.card__title').textContent = nameCard;
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));  
+  placesList.append(cardElement);
+};  
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const deleteCard = function(cardElement) {
+    cardElement.remove();
+};
+ 
+for (let i = 0; i < initialCards.length; i++) {
+   createCard(initialCards[i].link, initialCards[i].name, deleteCard);
+};
