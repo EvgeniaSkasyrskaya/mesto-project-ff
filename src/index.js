a51b578e16fe39e1c144d097f7f728a091496e51
EvgeniaSkasyrskaya <evgenia.skasyrskaya@yandex.ru>
2025-03-21 11:06:22 +0700

    2 часть проектной - исправление замечаний ревьюера

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import { initialCards } from './components/cards';
 import { createCard, deleteCard, likeCard } from './components/card';
 import { openModal, listenClick, closeModal } from './components/modal';
 
-//просмотр изображения карточки
+//объявление переменных для поиска DOM-элементов
+const placesList = document.querySelector('.places__list');
+
 const popupViewImage = document.querySelector('.popup_type_image');
+const popupImage = popupViewImage.querySelector('.popup__image');  
+const popupImageCaption = popupViewImage.querySelector('.popup__caption');
 
-const viewCardImage = (cardImage) => {
-  const popupImage = popupViewImage.querySelector('.popup__image');  
-  const popupCaption = popupViewImage.querySelector('.popup__caption');
-  popupImage.src = cardImage.src;
-  popupCaption.textContent = cardImage.closest('.card').querySelector('.card__title').textContent;
-  openModal(popupViewImage);
-};
-
-popupViewImage.addEventListener('click', (evt) => listenClick(evt, popupViewImage));
-
-
-//редактирование профиля
 const buttonEdit = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
 const formEditProfile = popupEdit.querySelector('.popup__form');
@@ -26,7 +18,21 @@ const inputProfileJob = formEditProfile.querySelector('.popup__input_type_descri
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
 
-const clearPopupEdit  = () => {
+const buttonAddCard = document.querySelector('.profile__add-button');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const formNewCard = popupNewCard.querySelector('.popup__form');
+const inputNewCardPlace = formNewCard.querySelector('.popup__input_type_card-name');
+const inputNewCardLink = formNewCard.querySelector('.popup__input_type_url');
+
+//объявление функций
+const viewCardImage = (cardImage) => {
+  popupImage.src = cardImage.src;
+  popupImage.alt = "пейзаж";
+  popupImageCaption.textContent = cardImage.closest('.card').querySelector('.card__title').textContent;
+  openModal(popupViewImage);
+};
+
+const fillFieldsFormEditProfile  = () => {
   inputProfileName.value = profileName.textContent;
   inputProfileJob.value = profileJob.textContent;
 };
@@ -37,26 +43,6 @@ const handleEditFormSubmit = (evt) => {
   profileJob.textContent = inputProfileJob.value;
   closeModal(popupEdit);
 };
-
-buttonEdit.addEventListener('click', () => {
-  openModal(popupEdit);
-  clearPopupEdit();
-});
-popupEdit.addEventListener('click', (evt) => listenClick(evt, popupEdit));
-formEditProfile.addEventListener('submit', handleEditFormSubmit);
-
-
-//добавление карточки
-const buttonAddCard = document.querySelector('.profile__add-button');
-const popupNewCard = document.querySelector('.popup_type_new-card');
-const formNewCard = popupNewCard.querySelector('.popup__form');
-const inputNewCardPlace = formNewCard.querySelector('.popup__input_type_card-name');
-const inputNewCardLink = formNewCard.querySelector('.popup__input_type_url');
-
-const clearPopupNewCard = () => {
-  inputNewCardPlace.value = "";
-  inputNewCardLink.value = "";
-};
   
 const handleNewCardFormSubmit = (evt) => {
   evt.preventDefault();
@@ -64,17 +50,31 @@ const handleNewCardFormSubmit = (evt) => {
   const placeImage = inputNewCardLink.value;
   placesList.prepend(createCard(placeImage, placeName, deleteCard, likeCard, viewCardImage));
   closeModal(popupNewCard);
-  clearPopupNewCard();
+  formNewCard.reset();
 };
 
 buttonAddCard.addEventListener('click', () => {
   openModal(popupNewCard);
-  clearPopupNewCard();
+  formNewCard.reset();
 });
 popupNewCard.addEventListener('click', (evt) => listenClick(evt, popupNewCard));
 formNewCard.addEventListener('submit', handleNewCardFormSubmit);
 
-
-//создание набора карточек
-const placesList = document.querySelector('.places__list');
+//вызов функций и методов, добавление обработчиков
 initialCards.forEach(element => {placesList.append(createCard(element.link, element.name, deleteCard, likeCard, viewCardImage))});
+
+popupViewImage.addEventListener('click', (evt) => listenClick(evt, popupViewImage));
+
+buttonEdit.addEventListener('click', () => {
+  openModal(popupEdit);
+  fillFieldsFormEditProfile();
+});
+popupEdit.addEventListener('click', (evt) => listenClick(evt, popupEdit));
+formEditProfile.addEventListener('submit', handleEditFormSubmit);
+
+buttonAddCard.addEventListener('click', () => {
+  openModal(popupNewCard);
+  formNewCard.reset();
+});
+popupNewCard.addEventListener('click', (evt) => listenClick(evt, popupNewCard));
+formNewCard.addEventListener('submit', handleNewCardFormSubmit);
